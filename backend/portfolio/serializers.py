dda6e11bddd1591dccb33f84827c3fcc805b9960
AshwinAkash24 <ashwinakash24@gmail.com>
2026-07-26 13:46:16 +0530

    fix: serialize MongoDB ObjectId as string to fix delete/edit on deployed site

--- backend/portfolio/serializers.py
+++ backend/portfolio/serializers.py
@@ -1,27 +1,42 @@
 from rest_framework import serializers
 from .models import Project, Skill, Experience, Certificate, Education
 
+
 class ProjectSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
     class Meta:
         model = Project
         fields = '__all__'
 
+
 class SkillSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
     class Meta:
         model = Skill
         fields = '__all__'
 
+
 class ExperienceSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
     class Meta:
         model = Experience
         fields = '__all__'
 
+
 class CertificateSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
     class Meta:
         model = Certificate
         fields = '__all__'
 
+
 class EducationSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
     class Meta:
         model = Education
         fields = '__all__'
